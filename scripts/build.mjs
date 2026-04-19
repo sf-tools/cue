2d745d61684e55bec9b4f495b5df21387b1d124f
theMackabu <theMackabu@gmail.com>
2026-04-19 03:32:54 -0700

    migrate to esbuild

---
+++ scripts/build.mjs
@@ -0,0 +1,15 @@
+import { build } from 'esbuild';
+
+await build({
+  entryPoints: ['src/cue.ts'],
+  bundle: true,
+  platform: 'node',
+  format: 'esm',
+  minify: true,
+  packages: 'external',
+  outfile: 'dist/cue.js',
+  tsconfig: 'tsconfig.json',
+  logLevel: 'info'
+});
+
+await import('./prepare-package-bin.mjs');
